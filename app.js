7034c210837698b22947e92b53d50d438f79bd08
Fernando Colom <colomfernando@gmail.com>
2019-07-22 21:15:53 -0300

    create class database

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
+/* eslint-disable node/no-unpublished-require */
 const yargs = require('yargs');
+const Notes = require('./lib/notes');
 
 yargs.command({
 	command: 'add',
 	describe: 'add note',
-	handler: function() {
-		console.log('add notee');
+	builder: {
+		title: {
+			describe: 'title for note',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'body of note',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function(argv) {
+		return Notes.add(argv);
 	}
 });
-console.log(yargs.argv);
+
+yargs.command({
+	command: 'remove',
+	describe: 'remove note',
+	handler: function() {
+		console.log('remove note');
+	}
+});
+
+yargs.command({
+	command: 'read',
+	describe: 'read note',
+	handler: function() {
+		console.log('reading note');
+	}
+});
+
+yargs.command({
+	command: 'list',
+	describe: 'list notes',
+	handler: function() {
+		return Notes.getNotes();
+	}
+});
+
+yargs.parse();
